fc368b87c9382504bf62b3556c02b6fdcfecea25
Asmaa Ahmed Marzouk <asmaaahmed23762@gmail.com>
2024-05-07 15:19:34 +0300

    filter with date and status in assignments

--- src/app/Components/Pages/Investigated Dashboard/Assignments/get-assignment/get-assignment.component.ts
+++ src/app/Components/Pages/Investigated Dashboard/Assignments/get-assignment/get-assignment.component.ts
@@ -5,44 +5,48 @@ import { AssignmentService } from '../../../../../Services/Assignments/assignmen
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { FilesService } from '../../../../../Services/Files/files.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-get-assignment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './get-assignment.component.html',
-  styleUrl: './get-assignment.component.scss'
+  styleUrl: './get-assignment.component.scss',
 })
 export class GetAssignmentComponent {
-  assignment: GetAssignment[]=[];
+  assignment: GetAssignment[] = [];
   currentTaskID: string = '';
-
+  filteredAssignments: GetAssignment[] = [];
+  filterDate: string = '';
+  filterStatus: string = '';
   constructor(
     private activatedrouter: ActivatedRoute,
     private router: Router,
     private assignmentservices: AssignmentService,
     private fileService: FilesService,
-    private http:HttpClient
+    private http: HttpClient
   ) {}
   ngOnInit(): void {
     this.currentTaskID = this.activatedrouter.snapshot.paramMap.get('ID') || '';
 
-    this.assignmentservices.getAssignmentByTaskID(this.currentTaskID).subscribe({
-      next: (currentTask) => {
-        console.log(currentTask);
-
-        this.assignment = currentTask;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    this.assignmentservices
+      .getAssignmentByTaskID(this.currentTaskID)
+      .subscribe({
+        next: (currentAssignments) => {
+          this.assignment = currentAssignments;
+          this.filteredAssignments = currentAssignments;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
   goBack() {
     this.router.navigate(['/TasksDashboard']);
   }
-  EditAssignment(id:string){
-    this.router.navigate(['/EditAssignment',id])
+  EditAssignment(id: string) {
+    this.router.navigate(['/EditAssignment', id]);
   }
   private extractFilename(url: string): string {
     // Extract filename from URL
@@ -50,7 +54,7 @@ export class GetAssignmentComponent {
   }
   DownloadFile(url: string): void {
     this.http.get(url, { responseType: 'blob' }).subscribe(
-      blob => {
+      (blob) => {
         const downloadURL = window.URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = downloadURL;
@@ -58,11 +62,17 @@ export class GetAssignmentComponent {
         link.click();
         window.URL.revokeObjectURL(downloadURL);
       },
-      error => {
+      (error) => {
         console.error('Error downloading the file:', error);
         // alert(`Failed to download the file. Server responded with status: ${error.status}`);
       }
     );
   }
-
+  applyFilters(): void {
+    this.filteredAssignments = this.assignment.filter(assignment => {
+      const matchesStatus = this.filterStatus ? assignment.status === this.filterStatus : true;
+      const matchesDate = this.filterDate ? new Date(assignment.created).toLocaleDateString() === new Date(this.filterDate).toLocaleDateString() : true;
+      return matchesStatus && matchesDate;
+    });
+  }
 }
